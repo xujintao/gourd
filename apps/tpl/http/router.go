b109dd269d255e13297cb3895576c10a05e25581
xujintao@126.com <xujintao@126.com>
2018-11-30 22:04:56 +0800

    add router

--- apps/tpl/http/router.go
+++ apps/tpl/http/router.go
@@ -2,18 +2,36 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/xujintao/gourd/apps/tpl/conf"
+	"github.com/xujintao/gourd/apps/tpl/http/middleware/session"
 )
 
 // SetupRouter 设置路由
 func SetupRouter() *gin.Engine {
-	//创建gin引擎
-	router := gin.Default()
-	router.Use(JwtAuthMiddleware(conf.Config.Auth.JWT.Secret))
 
-	// router.POST("/videos", NewVideo)
-	// router.GET("/videos", GetVideos)
-	// router.GET("/videos/:vid", GetVideo)
+	e := gin.Default()
+	e.Use(session.SetUser())
 
-	return router
+	// user
+	user := e.Group("/api/user", session.MustUser())
+	{
+		user.GET("", GetUser)
+		user.GET("/token", GetUserToken)
+
+		user.POST("/repos", SyncRepoList)
+		user.GET("/repos", GetRepoList)
+	}
+
+	// repo
+	repo := e.Group("/api/repos/:group/:project")
+	{
+		repo.GET("", GetRepo)
+		repo.GET("/builds", GetRepoBuildList)
+	}
+
+	// stream
+	{
+		// api.GET("/stream", GetStream)
+	}
+
+	return e
 }
